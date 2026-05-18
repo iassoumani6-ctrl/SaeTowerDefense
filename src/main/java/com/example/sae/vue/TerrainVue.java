0617bfe0b059976f4c80802004a567b7dbb58d9c
Imran ASSOUMANI <iassoumani@a205-04.iut.univ-paris8.fr>
2026-05-18 15:23:58 +0200

    Modif dans terrainvue

--- src/main/java/com/example/sae/vue/TerrainVue.java
+++ src/main/java/com/example/sae/vue/TerrainVue.java
@@ -20,14 +20,14 @@ public class TerrainVue {
     public void dessinerTerrain() {
         Image beige = new Image(Main.class.getResourceAsStream("/com/example/sae/image/beige.png"));
         Image bleu = new Image(Main.class.getResourceAsStream("/com/example/sae/image/bleu.png"));
-        Image cornerA1 = new Image(Main.class.getResourceAsStream("/com/example/sae/image/imageChemin/CornerA1.png"));
+        Image tribune1 = new Image(Main.class.getResourceAsStream("/com/example/sae/image/imageTribune/tribune1.png"));
 
         for (int ligne = 0; ligne < terrain.hauteur(); ligne++) {
             for (int col = 0; col < terrain.largeur(); col++) {
                 switch (terrain.codeTuile(ligne, col)){
                     case 1: tilePane.getChildren().add(new ImageView(beige)); break;
                     case 2: tilePane.getChildren().add(new ImageView(bleu)); break;
-                    case 3: tilePane.getChildren().add(new ImageView(cornerA1)); break;
+                    case 3: tilePane.getChildren().add(new ImageView(tribune1)); break;
                 }
 
             }
