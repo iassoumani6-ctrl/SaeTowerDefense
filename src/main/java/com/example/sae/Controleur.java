a855c97075458cbb5ec2576d147c3d824986f076
Samy Aitouche <saitouche@iut.univ-paris8.fr>
2026-05-21 14:43:07 +0200

    Refonte du deplacement de l'attaquant pour le faire deplacer sur les pixel. Ajout du Pane derriere le TilePane. Retirement des Ligne3 et 4 qui revenait en double dans les images et reorganisation du Terrain par rapport au nouveau code des images du a l'enlevement de Ligne3 et 4

--- src/main/java/com/example/sae/Controleur.java
+++ src/main/java/com/example/sae/Controleur.java
@@ -5,7 +5,11 @@ import com.example.sae.vue.TerrainVue;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.image.Image;
+<<<<<<< HEAD
 import javafx.scene.image.ImageView;
+=======
+import javafx.scene.layout.Pane;
+>>>>>>> 9112d81 (Refonte du deplacement de l'attaquant pour le faire deplacer sur les)
 import javafx.scene.layout.TilePane;
 
 import java.net.URL;
@@ -15,6 +19,8 @@ public class Controleur implements Initializable {
 
     @FXML
     private TilePane tilepane;
+    @FXML
+    private Pane paneJeu;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
@@ -24,12 +30,30 @@ public class Controleur implements Initializable {
         TerrainVue terrainVue = new TerrainVue(terrain, tilepane);
         terrainVue.dessinerTerrain();
 
+<<<<<<< HEAD
 
 
 
 
 
 
+=======
+        Attaquant attaquant = new Attaquant();
+
+        Image imageAttaquant = new Image(Main.class.getResourceAsStream("/com/example/sae/image/Ballon.png"));
+        AttaquantVue attaquantVue = new AttaquantVue(attaquant, paneJeu, imageAttaquant);
+        attaquantVue.mettreAJourPosition();
+
+        // Déplacer d'une case aléatoire toutes les 1 seconde
+        Timeline timeline = new Timeline(
+                new KeyFrame(Duration.seconds(0.010), event -> {
+                    attaquant.avancer();
+                    attaquantVue.mettreAJourPosition();
+                })
+        );
+        timeline.setCycleCount(Timeline.INDEFINITE);
+        timeline.play();
+>>>>>>> 9112d81 (Refonte du deplacement de l'attaquant pour le faire deplacer sur les)
 
     }
 }
--- src/main/java/com/example/sae/vue/TerrainVue.java
+++ src/main/java/com/example/sae/vue/TerrainVue.java
@@ -21,11 +21,141 @@ public class TerrainVue {
         Image bleu = new Image(Main.class.getResourceAsStream("bleu.png"));
         Image beige = new Image(Main.class.getResourceAsStream("beige.png"));
 
+<<<<<<< HEAD
         for (int ligne = 0; ligne < terrain.hauteur(); ligne++) {
             for (int col = 0; col < terrain.largeur(); col++) {
                 switch (terrain.codeTuile(ligne, col)){
                     case 1: tilePane.getChildren().add(new ImageView(bleu)); break;
                     case 2: tilePane.getChildren().add(new ImageView(beige)); break;
+=======
+        Image cornerA1 = new Image(Main.class.getResourceAsStream("/com/example/sae/image/imageChemin/CornerA1.png"));
+        Image cornerA2 = new Image(Main.class.getResourceAsStream("/com/example/sae/image/imageChemin/CornerA2.png"));
+        Image cornerA3 = new Image(Main.class.getResourceAsStream("/com/example/sae/image/imageChemin/CornerA3.png"));
+        Image cornerA4 = new Image(Main.class.getResourceAsStream("/com/example/sae/image/imageChemin/CornerA4.png"));
+
+        Image cornerB1 = new Image(Main.class.getResourceAsStream("/com/example/sae/image/imageChemin/CornerB1.png"));
+        Image cornerB2 = new Image(Main.class.getResourceAsStream("/com/example/sae/image/imageChemin/CornerB2.png"));
+        Image cornerB3 = new Image(Main.class.getResourceAsStream("/com/example/sae/image/imageChemin/CornerB3.png"));
+        Image cornerB4 = new Image(Main.class.getResourceAsStream("/com/example/sae/image/imageChemin/CornerB4.png"));
+
+        Image ligne1 = new Image(Main.class.getResourceAsStream("/com/example/sae/image/imageChemin/Ligne1.png"));
+        Image ligne2 = new Image(Main.class.getResourceAsStream("/com/example/sae/image/imageChemin/Ligne2.png"));
+        Image ligne3 = new Image(Main.class.getResourceAsStream("/com/example/sae/image/imageChemin/Ligne3.png"));
+        Image ligne4 = new Image(Main.class.getResourceAsStream("/com/example/sae/image/imageChemin/Ligne4.png"));
+
+        Image tribune1 = new Image(Main.class.getResourceAsStream("/com/example/sae/image/imageTribune/tribune_1.png"));
+        Image tribune2 = new Image(Main.class.getResourceAsStream("/com/example/sae/image/imageTribune/tribune_2.png"));
+        Image tribune3 = new Image(Main.class.getResourceAsStream("/com/example/sae/image/imageTribune/tribune_3.png"));
+        Image tribune4 = new Image(Main.class.getResourceAsStream("/com/example/sae/image/imageTribune/tribune_4.png"));
+        Image tribune5 = new Image(Main.class.getResourceAsStream("/com/example/sae/image/imageTribune/tribune_5.png"));
+
+        Image tribune6 = new Image(Main.class.getResourceAsStream("/com/example/sae/image/imageTribune/tribune_6.png"));
+        Image tribune7 = new Image(Main.class.getResourceAsStream("/com/example/sae/image/imageTribune/tribune_7.png"));
+        Image tribune8 = new Image(Main.class.getResourceAsStream("/com/example/sae/image/imageTribune/tribune_8.png"));
+        Image tribune9 = new Image(Main.class.getResourceAsStream("/com/example/sae/image/imageTribune/tribune_9.png"));
+        Image tribune10 = new Image(Main.class.getResourceAsStream("/com/example/sae/image/imageTribune/tribune_10.png"));
+
+        Image LogoMid1 = new Image(Main.class.getResourceAsStream("/com/example/sae/image/LogoMid1.png"));
+        Image LogoMid2 = new Image(Main.class.getResourceAsStream("/com/example/sae/image/LogoMid2.png"));
+        Image LogoMid3 = new Image(Main.class.getResourceAsStream("/com/example/sae/image/LogoMid3.png"));
+        Image LogoMid4 = new Image(Main.class.getResourceAsStream("/com/example/sae/image/LogoMid4.png"));
+
+        
+
+        for (int ligne = 0; ligne < terrain.getHauteur(); ligne++) {
+            for (int col = 0; col < terrain.getLargeur(); col++) {
+
+                Image imageTuile;
+
+                switch (terrain.getCodeTuile(ligne, col)) {
+                    case 0:
+                        imageTuile = beige;
+                        break;
+                    case 1:
+                        imageTuile = bleu;
+                        break;
+                    case 2:
+                        imageTuile = ligne1;
+                        break;
+                    case 3:
+                        imageTuile = ligne2;
+                        break;
+                    case 4:
+                        imageTuile = cornerA1;
+                        break;
+                    case 5:
+                        imageTuile = cornerA2;
+                        break;
+                    case 6:
+                        imageTuile = cornerA3;
+                        break;
+                    case 7:
+                        imageTuile = cornerA4;
+                        break;
+                    case 8:
+                        imageTuile = cornerB1;
+                        break;
+                    case 9:
+                        imageTuile = cornerB2;
+                        break;
+                    case 10:
+                        imageTuile = cornerB3;
+                        break;
+                    case 11:
+                        imageTuile = cornerB4;
+                        break;
+                    case 12:
+                        imageTuile = tribune1;
+                        break;
+                    case 13:
+                        imageTuile = ligne3;
+                        break;
+                    case 14:
+                        imageTuile = ligne4;
+                        break;
+                    case 15:
+                        imageTuile = tribune2;
+                        break;
+                    case 16:
+                        imageTuile = tribune3;
+                        break;
+                    case 17:
+                        imageTuile = tribune4;
+                        break;
+                    case 18:
+                        imageTuile = tribune5;
+                        break;
+                    case 19:
+                        imageTuile = tribune6;
+                        break;
+                    case 20:
+                        imageTuile = tribune7;
+                        break;
+                    case 21:
+                        imageTuile = tribune8;
+                        break;
+                    case 22:
+                        imageTuile = tribune9;
+                        break;
+                    case 23:
+                        imageTuile = tribune10;
+                        break;
+                    case 24:
+                        imageTuile = LogoMid1;
+                        break;
+                    case 25:
+                        imageTuile = LogoMid2;
+                        break;
+                    case 26:
+                        imageTuile = LogoMid3;
+                        break;
+                    case 27:
+                        imageTuile = LogoMid4;
+                        break;
+                    default:
+                        imageTuile = beige;
+                        break;
+>>>>>>> 9112d81 (Refonte du deplacement de l'attaquant pour le faire deplacer sur les)
                 }
 
             }
