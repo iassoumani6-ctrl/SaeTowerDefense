feaded975210ed62903f8f18b284b99bcbfee1f9
Imran <iassoumani@a205-04.iut.univ-paris8.fr>
2026-05-21 12:02:15 +0200

    Ajout des différents ennemies qui extends attaquan

---
+++ src/main/java/com/example/sae/modele/ennemis/Boss.java
@@ -0,0 +1,26 @@
+package com.example.sae.modele.ennemis;
+
+import com.example.sae.modele.Attaquant;
+import com.example.sae.modele.Terrain;
+
+public class Boss extends Attaquant {
+
+    private int pv;
+    private int degats;
+    private int vitesse;
+
+    public Boss (int ligneDep, int colonneDep, Terrain terrain) {
+        super(ligneDep, colonneDep, terrain);
+        this.pv = 60;
+        this.degats = 10;
+        this.vitesse = 3;
+    }
+    public int getDegats() {
+        return degats;
+    }
+
+    public int getPv() {
+        return pv;
+    }
+
+}
