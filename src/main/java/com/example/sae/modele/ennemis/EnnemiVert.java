43a57802b1876b7a2931695c044beb9387e8f009
Stiven Niculica <sniculica@iut.univ-paris8.fr>
2026-05-21 11:21:17 +0200

    Modification de l'interface utilisateur via fxml

--- src/main/java/com/example/sae/modele/ennemis/EnnemiVert.java
+++ src/main/java/com/example/sae/modele/ennemis/EnnemiVert.java
@@ -1,19 +1,23 @@
 package com.example.sae.modele.ennemis;
 import com.example.sae.modele.Attaquant;
 import com.example.sae.modele.Terrain;
-
+/*
+*   Ennemi le plus faible
+* */
 public class EnnemiVert extends Attaquant {
     private int ligne;
     private int colonne;
     private Terrain terrain;
     private int pv;
     private int degats;
+    private int vitesse;
 
 
     public EnnemiVert(int ligneDep, int colonneDep, Terrain terrain) {
         super(ligneDep, colonneDep, terrain);
         this.pv = 50;
         this.degats = 5;
+        this.vitesse = 3;
     }
 
     public int getPv() { return this.pv; }
