1f1488b2faa110b4e45fde0398508088ba6293a0
Stiven Niculica <sniculica@iut.univ-paris8.fr>
2026-06-10 15:04:18 +0200

    modif gesitonnairevague + ajout des images correscpondates aux différentes tours

--- src/main/java/com/example/sae/Controleur.java
+++ src/main/java/com/example/sae/Controleur.java
@@ -4,7 +4,7 @@ import com.example.sae.modele.Ballon;
 import com.example.sae.modele.GestionnaireVagues;
 import com.example.sae.modele.Terrain;
 import com.example.sae.modele.Tour;
-import com.example.sae.modele.defenseurs.Shifty;
+import com.example.sae.modele.defenseurs.*;
 import com.example.sae.modele.ennemis.*;
 import com.example.sae.vue.BallonVue;
 import com.example.sae.vue.TerrainVue;
@@ -17,12 +17,14 @@ import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.layout.Pane;
 import javafx.util.Duration;
-import com.example.sae.modele.defenseurs.Laser;
+
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
 
 public class Controleur implements Initializable {
 
@@ -50,7 +52,7 @@ public class Controleur implements Initializable {
         terrainVue.dessinerTerrain();
         gestionnaireVagues = new GestionnaireVagues();
 
-        imageTour      = new Image(Main.class.getResourceAsStream("/com/example/sae/image/Laser.png"));
+//        imageTour      = new Image(Main.class.getResourceAsStream("/com/example/sae/image/Laser.png"));
 
         ajouterEnnemi(new BallonVert());
 
@@ -94,9 +96,7 @@ public class Controleur implements Initializable {
             gestionnaireVagues.signalerVagueFinie();
         }
 
-        if (vagueLabel != null) {
-            vagueLabel.setText(String.valueOf(gestionnaireVagues.getNumeroVague()));
-        }
+        vagueLabel.setText(String.valueOf(gestionnaireVagues.getNumeroVague()));
 
         for (BallonVue av : ballonVues) {
             av.getBallon().avancer();
@@ -146,7 +146,7 @@ public class Controleur implements Initializable {
     }
 
     @FXML
-    private void ajouterTour() {
+    private void ajouterTour(ActionEvent event) {
         if (colSelectionnee == -1 || ligneSelectionnee == -1) {
             System.out.println("Sélectionnez une case valide d'abord !");
             return;
@@ -156,8 +156,16 @@ public class Controleur implements Initializable {
             System.out.println("Impossible de placer la tour ici !");
             return;
         }
+        String type = ((Button) event.getSource()).getText();
 
-        Tour tour = new Shifty(colSelectionnee, ligneSelectionnee);
+        Tour tour = switch (type) {
+            case "Shooter"      -> new Shooter(colSelectionnee, ligneSelectionnee);
+            case "Zoner"        -> new Zoner(colSelectionnee, ligneSelectionnee);
+            case "Ralentisseur" -> new Ralentisseur(colSelectionnee, ligneSelectionnee);
+            case "Laser"        -> new Laser(colSelectionnee, ligneSelectionnee);
+            case "Canonner"     -> new Canonner(colSelectionnee, ligneSelectionnee);
+            default             -> new Shifty(colSelectionnee, ligneSelectionnee);
+        };
 
         terrain.occuperCasesTour(tour);
         tours.add(tour);
